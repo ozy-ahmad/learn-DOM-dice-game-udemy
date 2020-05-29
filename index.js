84a30bf49bb8e4acb0e0417601b11a1384c1776a
Ahmad Fakhrozy <ahmadfakhrozy@gmail.com>
2020-05-29 13:59:51 +0700

    fix dom and style

--- index.js
+++ index.js
@@ -7,17 +7,16 @@ let image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
-let randomNumber2 = Math.floor(Math.random() * 6) +1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-let randomImageSource2 = "images/dice" + randomNumber2 +".png";
+let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 if (randomNUmber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 1 Wins!";
-} else if (randomNumber2 > randomNUmber1){
-    document.querySelector("h1").innerHTML = "Player 2 Wins!"
-} 
-else {
-    document.querySelector("h1").innerHTML = "Draw!"
+  document.querySelector("h2").innerHTML = "Player 1 Wins!";
+} else if (randomNumber2 > randomNUmber1) {
+  document.querySelector("h2").innerHTML = "Player 2 Wins!";
+} else {
+  document.querySelector("h2").innerHTML = "Draw!";
 }
